d034bb1e4f81d117c1f1081eb7872b25c1710c63
Abiodun   [Wale] Ajagunna <ajagunnaabiodun752@yahoo.com>
2019-11-08 05:43:58 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -70,7 +70,10 @@ def adding_matz
 # }
 
 	programmer_hash = 
- 		{
+ 		{:yukihiro_matsumoto => {
+ 		    :known_for => "Ruby",
+ 		    :languages => ["LISP", "C"]
+       }
         :grace_hopper => {
           :known_for => "COBOL",
           :languages => ["COBOL", "FORTRAN"]
@@ -85,7 +88,7 @@ def adding_matz
         }
      }
 
-    programmer_hash[:yukihiro_matsumoto] << [:grace_hopper, :alan_kay, :dennis_ritchie]
+    
 end
 
 def changing_alan
