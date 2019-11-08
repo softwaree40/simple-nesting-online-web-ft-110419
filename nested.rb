69b93d4bddb1d98881832172637990707ef0b764
Abiodun   [Wale] Ajagunna <ajagunnaabiodun752@yahoo.com>
2019-11-08 06:03:33 +0000

    Done.

--- nested.rb
+++ nested.rb
@@ -1,4 +1,4 @@
-
+require 'pry'
 def hopper
 	programmer_hash = 
  		{
@@ -70,10 +70,13 @@ def adding_matz
 # }
 
 	programmer_hash = 
- 		{
+ 		{:yukihiro_matsumoto => {
+ 		    :known_for => "Ruby",
+ 		    :languages => ["LISP", "C"]
+       },
         :grace_hopper => {
           :known_for => "COBOL",
-          :languages => ["COBOL", "FORTRAN"]
+          :languages => ["COBOL", "FORTRAN"],
         },
         :alan_kay => {
           :known_for => "Object Orientation",
@@ -106,8 +109,9 @@ def changing_alan
      }
      #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
      alans_new_info = "GUI"
-     
-     
+     programmer_hash[:alan_kay][:known_for] = "GUI"
+     #binding.pry
+     programmer_hash
 end
 
 def adding_to_dennis
@@ -126,5 +130,6 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
-
+    programmer_hash[:dennis_ritchie][:languages] << 'Assembly'
+    programmer_hash
 end
